f6402e60c418cff8683d04f2e53d9b54632537e3
bhavyagithub05 <jainbhavya552@gmail.com>
2026-01-30 12:14:55 +0530

    change image name again

--- client/src/assets/img.js
+++ client/src/assets/img.js
@@ -1,6 +1,6 @@
 import image1 from './image1.png'
 import image2 from './image2.jpg'
-import logo from './logo.png'
+import logo from './Logo.png'
 export const assest = { 
     image1,
     image2,
